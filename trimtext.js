b8b0b887a2ebed298d119b56469764e77bd70c8f
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-03-30 08:21:44 +0530

    replace with smaller grades

---
+++ trimtext.js
@@ -0,0 +1,23 @@
+
+
+const fs = require('fs');
+const path = require('path');
+
+
+async function test(){
+
+    let mypath =  path.join(__dirname,'hadithfiles')
+    let files = fs.readdirSync(mypath)
+
+    for(let file of files){
+      let filePath = path.join(mypath,file)
+        let str = fs.readFileSync(filePath).toString()
+        let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
+
+        //console.log(file,arr.slice(0,10))
+        fs.writeFileSync(filePath,arr.join('\n').trim())
+    }
+    
+
+}
+test()
