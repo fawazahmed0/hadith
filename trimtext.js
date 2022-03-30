c80c4afa6a3d6dfa1b569b6c8b36fb006016f330
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-03-31 04:37:02 +0530

    replace urdu/persian characters with arabic one

--- trimtext.js
+++ trimtext.js
@@ -11,7 +11,7 @@ async function test(){
 
     for(let file of files){
       let filePath = path.join(mypath,file)
-        let str = fs.readFileSync(filePath).toString().normalize("NFD").replace(/\p{Diacritic}/gu, "")
+        let str = fs.readFileSync(filePath).toString().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace(/\u06CC/g,'\u064A').replace(/\u06C1/g,'\u0647')
         let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
 
         //console.log(file,arr.slice(0,10))
