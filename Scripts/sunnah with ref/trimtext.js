02768d07bea1befe85c3363de3ac1a8db0c33b1b
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-02 16:28:32 +0530

    remove commas and trim text

--- Scripts/sunnah with ref/trimtext.js
+++ Scripts/sunnah with ref/trimtext.js
@@ -6,12 +6,12 @@ const path = require('path');
 
 async function test(){
 
-    let mypath =  path.join(__dirname,'hadithfiles')
+    let mypath =  path.join(__dirname,'scrapped')
     let files = fs.readdirSync(mypath)
 
     for(let file of files){
       let filePath = path.join(mypath,file)
-        let str = fs.readFileSync(filePath).toString().normalize("NFD").replace(/\p{Diacritic}/gu, "").replace(/\u06CC/g,'\u064A').replace(/\u06C1/g,'\u0647')
+        let str = fs.readFileSync(filePath).toString() //.replace(/(\d+ \|)\s+\,\s+/g,'$1 ')
         let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
 
         //console.log(file,arr.slice(0,10))
