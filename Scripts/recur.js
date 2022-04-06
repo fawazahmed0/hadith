9141a4a9b7c1a5fcca03cc7beb3af7267ad32aa8
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-06 18:08:58 +0530

    Update recur.js

--- Scripts/recur.js
+++ Scripts/recur.js
@@ -16,13 +16,15 @@ async function test(){
 
             for(let i=0;i<arr.length;i++){
                 try{
-                let prevVal = parseFloat( arr[i-1].match(/\d+\.?\d*/)[0] )
+                let prevVal = parseFloat( arr[i-10].match(/\d+\.?\d*/)[0] )
                 let currentVal = parseFloat( arr[i].match(/\d+\.?\d*/)[0])
-                let nextVal = parseFloat( arr[i+1].match(/\d+\.?\d*/)[0])
+                let nextVal = parseFloat( arr[i+10].match(/\d+\.?\d*/)[0])
 
-                if(Math.abs(prevVal - currentVal) > 10 && Math.abs(currentVal - nextVal) > 10 && Math.abs(prevVal - nextVal)<7 ){
+                if(Math.abs(prevVal - currentVal) > 25 && Math.abs(currentVal - nextVal) > 25 && Math.abs(prevVal - nextVal)<25 ){
+
                     currentVal = (prevVal + nextVal)/2
                     currentVal = Number.isInteger(currentVal) ? currentVal : currentVal.toFixed(1)
+                    if(Number.isInteger(currentVal))
                     arr[i] = currentVal + arr[i].replace(/\d+\.?\d*/,'')
                 }
                     
