aaf9718a273aec23f74cddfa4ffd1d4fcdefefbb
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-06 12:43:22 +0530

    Create replacenums.js

---
+++ Scripts/bangla/replacenums.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+
+async function test(){
+
+    let mypath =  path.join(__dirname,'compiled')
+    let files = fs.readdirSync(mypath)
+
+    for(let file of files){
+      let filePath = path.join(mypath,file)
+        let str = fs.readFileSync(filePath).toString()
+        let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
+        for(let i=0;i<arr.length;i++){
+            arr[i] = arr[i].split(' | ')[0].trim() + ' | ' + arr[i].split(' | ').slice(1).join(' ').replace(/^[০-৯]+\./mg,"").trim()
+        }
+        fs.writeFileSync(filePath,arr.join('\n').trim())
+    }
+}
+test()
