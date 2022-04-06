2f364a657a0083eb07a1692c1fab6a933f534b72
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-06 08:06:31 +0530

    nasai

--- Scripts/bangla/cleanup.js
+++ Scripts/bangla/cleanup.js
@@ -10,12 +10,15 @@ async function test(){
     let files = fs.readdirSync(mypath)
 
     for(let file of files){
+        console.log(file)
+        if(!file.includes('nasai.txt'))
+        continue
       let filePath = path.join(mypath,file)
         let str = fs.readFileSync(filePath).toString()
         let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
         arr = arr.map(e=>{
             try{
-            return e.split(/\s{60,}/g)[0].split(' | ')[0].trim() +' | ' + e.split(/\s{60,}/g)[1].trim()
+            return e.split(/\s{30,}/g)[0].split(' | ')[0].trim() +' | ' + e.split(/\s{30,}/g).slice(1).join(' ').trim()
             }catch(e){}
             return e
         })
