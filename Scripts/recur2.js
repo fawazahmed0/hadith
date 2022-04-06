f7c4c427344d6a364f4b25b5f671a1298951fbd3
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-06 14:31:29 +0530

    Update recur2.js

--- Scripts/recur2.js
+++ Scripts/recur2.js
@@ -16,7 +16,7 @@ async function test(){
             let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.split(/\s+\|\s+/))
             for(let i=0;i<arr.length;i++){
 
-                if(Math.abs(arr[i][0]-arr[i+1][0])>5 && (Math.abs(arr[i][0]-arr[i+2][0])==2 || (Math.abs(arr[i][0]-arr[i+2][0])==0.1)){
+                if(Math.abs(arr[i][0]-arr[i+1][0])>5 && (Math.abs(arr[i][0]-arr[i+2][0])==2 || Math.abs(arr[i][0]-arr[i+2][0])==0.1)){
                     arr[i+1][0] = (arr[i][0]+arr[i+2][0])/2
                 }
 
