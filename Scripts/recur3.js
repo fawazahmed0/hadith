537a6f6545ae831d17578fddaf2abaf4170305e1
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-07 01:25:38 +0530

    Update recur3.js

--- Scripts/recur3.js
+++ Scripts/recur3.js
@@ -4,19 +4,50 @@ let fs = require('fs');
 
 
 let filesArr  = []
+
+let lasthadithObj = {bukhari :7563,abudawud: 5274,
+
+    tirmidhi: 3956,nasai: 5758,
+    
+    muslim: 3033.2,malik: 1858,
+    ibnmajah: 4341}
+
 async function test(){
     
     traverseDir(path.join(__dirname))
     
     for(let filePath of filesArr){
 
-        if(filePath.includes('compiled')){
-            let str = fs.readFileSync(filePath).toString()
-            let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
+        if(!filePath.includes('compiled'))
+        continue
+
+        let str = fs.readFileSync(filePath).toString()
+        let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim())
+
+        for(let [key,value] of Object.entries(lasthadithObj)){
+            if(filePath.includes(key)){
+                if(arr.slice(-1)[0].match(/\d+/)[0] !=value){
+                    console.log("wrong last hadith no with "+filePath)
+                    continue
+                }
+
+            }
+
+        }
+
+
 
             for(let i=arr.length;i>0;i--){
-                for(let j=i+1;j<i+10;j++){
+                let refVal = parseFloat( arr[i].match(/\d+\.?\d*/)[0])
+                let nextRefVal = parseFloat( arr[i-1].match(/\d+\.?\d*/)[0])
+                let diff = refVal - nextRefVal
+                for(let j=i-2;j>i-10 && diff==1 ;j--){
                     try{
+                        let currentVal = parseFloat( arr[j].match(/\d+\.?\d*/)[0])
+                        if(refVal-currentVal>10+j-i+2){
+                            let newVal = refVal - i-j
+                            arr[j] = newVal + arr[j].replace(/\d+\.?\d*/,'')
+                        }
 
                     
                     
@@ -26,7 +57,7 @@ async function test(){
                
             }
             fs.writeFileSync(filePath,arr.join('\n').trim())
-        }
+        
 
 
 
