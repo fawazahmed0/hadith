f40446be47b9da0925cd0c55c19a964a239fa6c3
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-09 12:56:21 +0530

    create grades json

---
+++ grades/creategradesjson.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+
+async function test(){
+    let myjson = {}
+    let mapjson = JSON.parse(fs.readFileSync(path.join(__dirname,'engaramap.json')).toString())
+    let mypath =  path.join(__dirname,'hadithfiles')
+    let folders = fs.readdirSync(mypath)
+
+    for(let folder of folders){
+        let files = fs.readdirSync(path.join(mypath,folder))
+
+        if(!myjson[folder])
+        myjson[folder] = {"hadiths":{}} 
+
+        for(let file of files){
+            let filePath = path.join(mypath,folder,file)
+              let str = fs.readFileSync(filePath).toString()
+              let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem))
+                for(let val of arr){
+                    let [num, grade] = val.split('|').map(e=>e.trim())
+                    if(!myjson[folder]["hadiths"][num])
+                    myjson[folder]['hadiths'][num] = {}
+                    myjson[folder]['hadiths'][num]['hadithno'] = parseFloat(num)
+                    if(!myjson[folder]['hadiths'][num]['grades'])
+                    myjson[folder]['hadiths'][num]['grades'] = []
+
+
+                    
+                    let tempjson = {}
+                    tempjson['name'] = file.replace('.txt','').trim()
+                    tempjson['grade'] = mapjson[grade]
+                    myjson[folder]['hadiths'][num]['grades'].push(tempjson)
+
+                }
+    
+          }
+
+    }
+
+    fs.writeFileSync(path.join(__dirname, 'grades.json'),JSON.stringify(myjson,null,4))
+
+
+}
+test()
