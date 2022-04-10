97f23bf75a16935ab21a2bed4b89dc096439434a
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-11 03:25:45 +0530

    add muslim in gradesjson

--- grades/musbukref.js
+++ grades/musbukref.js
@@ -3,18 +3,30 @@ const fs = require('fs');
 const path = require('path')
 async function test(){
     // 'muslim',
-   let refFolders =  ['bukhari']
+   let refFolders =  ['muslim']
 
    for(let refval of refFolders){
     let str = fs.readFileSync(path.join(__dirname,'ref',refval+'reference.txt')).toString()
     let gradesJSON = JSON.parse(fs.readFileSync(path.join(__dirname,'grades.json')).toString())
 
-    let refArr = [...str.matchAll(/(\d+)In\-book\s*reference\s*\:\s*Book\s*(\d+)\,\s*hadith\s*(\d+)/ig)]
+    let mapJSON = JSON.parse(fs.readFileSync(path.join(__dirname,'..','Scripts','dotmappingmerged.json')).toString())
+
+    let refArr = [...str.matchAll(/(\d+[a-z]?)In\-book\s*reference\s*\:\s*Book\s*(\d+)\,\s*hadith\s*(\d+)/ig)]
   
 
     for(let ref of refArr){
-        if(gradesJSON?.["editions"]?.[refval]?.['hadiths']?.[ref[1]])
-        gradesJSON["editions"][refval]['hadiths'][ref[1]]['bookref'] = {"book":parseFloat(ref[2]),"hadith":parseFloat(ref[3])}
+        
+        let num = getKeyByValue(mapJSON, alpha2num(ref[1]))
+        //console.log('num is ',num,'alpha is ',ref[1],'alpha2num is',alpha2num(ref[1]))
+        if(gradesJSON?.["editions"]?.[refval]?.['hadiths']?.[num]){
+        gradesJSON["editions"][refval]['hadiths'][num]['bookref'] = {"book":parseFloat(ref[2]),"hadith":parseFloat(ref[3])}
+        if(mapJSON[num])
+        gradesJSON["editions"][refval]['hadiths'][num]['arabicnumber'] = mapJSON[num]
+        else{
+        gradesJSON["editions"][refval]['hadiths'][num]['arabicnumber'] = ''
+            console.log('undeinfed')
+    }
+        }
         
     }
 
@@ -34,7 +46,18 @@ async function test(){
 
     test()
  
+    function alpha2num(str){
+        let val =  str.match(/(\d+)([a-z])/)
+         if(val)
+             return val[1] +'.' + (val[2].charCodeAt(0)-96).toString().padStart(2,'0')
+             return str.toString()
+      }
+      
+      function getKeyByValue(object, value) {
+        return Object.keys(object).find(key => object[key] == value);
+      }
 
+    
 
 
 
