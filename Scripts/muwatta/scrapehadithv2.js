3bc38dd61f97ad449363cfb96f7f6950358894e1
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-11 15:34:00 +0530

    fetch book names

--- Scripts/muwatta/scrapehadithv2.js
+++ Scripts/muwatta/scrapehadithv2.js
@@ -3,45 +3,25 @@
 
 const { firefox } = require('playwright');
 const fs = require('fs');
+const path = require('path');
 
 async function test(){
 
     const browser = await firefox.launch({ headless: true});
     const context = await browser.newContext();
     const page = await context.newPage();
-    let hadithbooks = ["malik"]
+    let hadithbooks = ["bukhari","muslim","nasai","abudawud","tirmidhi","ibnmajah","malik"]
   //  await page.goto('https://sunnah.com/bukhari/12')
     //await page.evaluate(() => toggleLanguageDisplay('urdu'))
 
     for(let book of hadithbooks){
-        let str = "";
-        let ref = "";
 
-        let str1 = ""
-        let ref1 = ""
-    for(let i=1;i<=1000;i++){
+        await page.goto('https://sunnah.com/'+book)
+        
+     let arr =    await page.evaluate(() => Array.from(document.querySelectorAll('.english_book_name')).map(e=>[e.parentNode.querySelector('.title_number').textContent,e.parentNode.querySelector('.english_book_name').textContent]))
+        
+    fs.writeFileSync(path.join(__dirname, book+'booksnames.json'),JSON.stringify(Object.fromEntries(arr.filter(e=>e[0]!=''))))
 
-        let res = await page.goto('https://sunnah.com/'+book+'/'+i)
-        if(!res.ok())
-        break
-        await page.waitForTimeout(4000)
-        str ='\n'+str+'\n'+await page.evaluate(() =>Array.from(document.querySelectorAll('.actualHadithContainer')).map(e=>{
-            let urdu = e.querySelector('.english_hadith_full') ? e.querySelector('.english_hadith_full').textContent : ''
-            return urdu.replaceAll('\n',' ') + '\n'+e.querySelector('.hadith_reference').textContent.replaceAll('\n',' ')+'\n\n'
-        }))
-        ref ='\n'+ref+'\n'+await page.evaluate(() =>Array.from(document.querySelectorAll('.actualHadithContainer')).map(e=>e.querySelector('.hadith_reference').textContent.replaceAll('\n',' ')+'\n\n'))
-      
-        str1 ='\n'+str1+'\n'+await page.evaluate(() =>Array.from(document.querySelectorAll('.actualHadithContainer')).map(e=>e.querySelector('.arabic_hadith_full').textContent.replaceAll('\n',' ') + '\n'+e.querySelector('.hadith_reference').textContent.replaceAll('\n',' ')+'\n\n'))
-        ref1 ='\n'+ref1+'\n'+await page.evaluate(() =>Array.from(document.querySelectorAll('.actualHadithContainer')).map(e=>e.querySelector('.hadith_reference').textContent.replaceAll('\n',' ')+'\n\n'))
-
-    }
-   
-
-    fs.writeFileSync(book+'englishscrapped.txt',str)
-    fs.writeFileSync(book+'englishreference.txt',ref)
-
-    fs.writeFileSync(book+'arabicscrapped.txt',str1)
-    fs.writeFileSync(book+'arabicreference.txt',ref1)
 }
     await browser.close();
   
