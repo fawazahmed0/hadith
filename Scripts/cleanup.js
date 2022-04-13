6fdf38052fd75a3b3a43f0b8d33fdd8704cfb6fc
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-13 15:22:32 +0530

    sort

--- Scripts/cleanup.js
+++ Scripts/cleanup.js
@@ -15,13 +15,13 @@ async function test(){
             console.log(filePath)
             let str = fs.readFileSync(filePath).toString()
             let arr = str.split(/\r?\n/).filter(elem => !/^\s*$/.test(elem)).map(e=>e.split('|')[0].trim()+' | '+e.split('|').slice(1).join(' ').trim())
-            //arr.sort((a, b) => parseFloat(a.match(/\d+\.?\d*/)[0]) - parseFloat(b.match(/\d+\.?\d*/)[0]))
+            arr.sort((a, b) => parseFloat(a.match(/\d+\.?\d*/)[0]) - parseFloat(b.match(/\d+\.?\d*/)[0]))
             
-            let duplicates = arr.map(e=>e.match(/\d+\.?\d*/)[0]).filter((e, i, a) => a.indexOf(e) !== i)
-           for(let i=0;i<arr.length;i++){
-                if(duplicates.includes(arr[i].match(/\d+\.?\d*/)[0]))
-                arr[i] = ''
-            }
+         //   let duplicates = arr.map(e=>e.match(/\d+\.?\d*/)[0]).filter((e, i, a) => a.indexOf(e) !== i)
+         //  for(let i=0;i<arr.length;i++){
+       //         if(duplicates.includes(arr[i].match(/\d+\.?\d*/)[0]))
+      //          arr[i] = ''
+      //      }
        
             fs.writeFileSync(filePath,arr.filter(elem => !/^\s*$/.test(elem)).map(e=>e.trim()).join('\n').replace(/\s\s+/g,' ').trim())
         
