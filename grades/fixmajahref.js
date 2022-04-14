d56e72211b1ac2c40dd71fd5fbefcc813eb27a90
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-14 16:26:10 +0530

    fix numberings

--- grades/fixmajahref.js
+++ grades/fixmajahref.js
@@ -6,12 +6,15 @@ async function test(){
     let gradesJSON = JSON.parse(fs.readFileSync(gradesPath).toString())
     let bookname = 'muslim'
     for(let i=0;i<gradesJSON[bookname]['hadiths'].length;i++){
-        
+        if(gradesJSON[bookname]['hadiths'][i]["hadithnumber"] == gradesJSON[bookname]['hadiths'][i]["arabicnumber"])
+        gradesJSON[bookname]['hadiths'][i]["arabicnumber"] = 0
+        /*
         if(!gradesJSON[bookname]['hadiths'][i]["reference"] && gradesJSON[bookname]['hadiths'][i]["hadithnumber"]<93){
             gradesJSON[bookname]['hadiths'][i]["reference"] = {}
             gradesJSON[bookname]['hadiths'][i]["reference"]["book"] = 0
             gradesJSON[bookname]['hadiths'][i]["reference"]["hadith"] = gradesJSON[bookname]['hadiths'][i]["hadithnumber"]
         }
+        */
     }
 
 
