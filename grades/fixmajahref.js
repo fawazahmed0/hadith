3eeaa6fca1182ef453d7b8d196f81eeb512f2da4
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-04-14 15:56:18 +0530

    fix muslim ref

--- grades/fixmajahref.js
+++ grades/fixmajahref.js
@@ -4,12 +4,13 @@ const path = require('path')
 async function test(){
     let gradesPath = path.join(__dirname,'..','..','hadith-api','info.json')
     let gradesJSON = JSON.parse(fs.readFileSync(gradesPath).toString())
-
-    for(let i=0;i<gradesJSON['ibnmajah']['hadiths'].length;i++){
-        if(!gradesJSON['ibnmajah']['hadiths'][i]["reference"] && gradesJSON['ibnmajah']['hadiths'][i]["hadithnumber"]<300){
-            gradesJSON['ibnmajah']['hadiths'][i]["reference"] = {}
-            gradesJSON['ibnmajah']['hadiths'][i]["reference"]["book"] = 0
-            gradesJSON['ibnmajah']['hadiths'][i]["reference"]["hadith"] = gradesJSON['ibnmajah']['hadiths'][i]["hadithnumber"]
+    let bookname = 'muslim'
+    for(let i=0;i<gradesJSON[bookname]['hadiths'].length;i++){
+        
+        if(!gradesJSON[bookname]['hadiths'][i]["reference"] && gradesJSON[bookname]['hadiths'][i]["hadithnumber"]<93){
+            gradesJSON[bookname]['hadiths'][i]["reference"] = {}
+            gradesJSON[bookname]['hadiths'][i]["reference"]["book"] = 0
+            gradesJSON[bookname]['hadiths'][i]["reference"]["hadith"] = gradesJSON[bookname]['hadiths'][i]["hadithnumber"]
         }
     }
 
