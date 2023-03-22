11b174d87998572bb29d0bc327b4059cde9f7a98
Fawaz Ahmed <fawazahmed0@hotmail.com>
2023-03-22 23:40:20 +0530

    add

--- Scripts/bengali/testsearch.js
+++ Scripts/bengali/testsearch.js
@@ -1,11 +1,11 @@
 async function test(){
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
     
-    let indexname = "abudawudbengali"
+    let indexname = "abudawudarabic"
     let jsonval = {
         "query": {
           "match": {
-            "column2": "حدثنا مسدد بن مسرهد، حدثنا عيسى بن يونس، أخبرنا إسماعيل بن عبد الملك، عن أبي الزبير، عن جابر بن عبد الله، أن النبي صلي الله عليه وسلم كان إذا أراد البراز انطلق حتى لا يراه أحد ‏\n- صحيح \n"
+            "column2": "حدثنا مسدد، حدثنا عبد الله بن داود، عن سلمة بن نبيط، عن رجل، من الحي، عن أبيه نبيط، أنه رأى النبي صلى الله عليه وسلم واقفا بعرفة على بعير أحمر يخط"
           }
         }
       }
@@ -16,9 +16,9 @@ async function test(){
          "Accept": "application/json"},
          body:JSON.stringify(jsonval)
        }
-    let res = await fetch("https://localhost:9200/"+indexname+"/_search", requestOptions)
+    let res = await fetch("https://192.168.1.192:9200/"+indexname+"/_search", requestOptions)
     let data = await res.json()
-    console.log(data)
+    console.log(JSON.stringify(data,null,4))
 
 }
 
---
+++ grades/zubairzai-genindex.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+
+async function test(){
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    let mypath =  path.join('G:\\', 'hadith','alizai')
+    let folders = fs.readdirSync(mypath).filter(e=>e!='fetc.js')
+    let indexmap = {"abudawood":"abudawud","ibnemaja":"ibnmajah","nasai":"nasai","tirmizi":"tirmidhi"}
+
+    for(let folder of folders){
+        let files = fs.readdirSync(path.join(mypath,folder))
+        let sortedFiles = files.map(e=>parseInt(e.split('.')[0])).sort((a, b) => (a - b))
+        let lasthadithno = 1
+        console.log(folder)
+
+        let myjson = {}
+
+        for(let fileno of sortedFiles){
+
+            let str = fs.readFileSync(path.join(mypath,folder,fileno+'.js')).toString()
+            eval(str.replace('var',''))
+            
+
+            let indexname = `${indexmap[folder]}arabic`
+            let jsonval = {
+                "query": {
+                    "match": {
+                        "column2": global[folder].arabic.normalize("NFD").replace(/\p{Diacritic}|\p{Mark}|\p{Extender}|\p{Bidi_Control}/gu, "")
+                    }
+                }
+            }
+
+            var requestOptions = {
+                method: "POST",
+                headers: {
+                    "Authorization": "Basic ZWxhc3RpYzpXekswNXRTVVh4UzZibUlZRHhzOQ==",
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(jsonval)
+            }
+            let res = await fetch("https://192.168.1.192:9200/" + indexname + "/_search", requestOptions)
+            let resdata = await res.json()
+
+
+            let columns = resdata.hits.hits.filter(e => e._score > 20 &&  lasthadithno-e._source.column1>=0 && lasthadithno-e._source.column1 <10).slice(0, 3).map(e => e._source.column1)
+
+            if (lasthadithno != 1)
+                columns = columns.filter(e => e != lasthadithno)
+
+            let grade = global[folder]?.hukam || global[folder]?.description || ''
+
+            if (columns.length == 0 || !grade)
+                continue
+               
+
+            let newhadithno = columns.reduce((prev, curr) => Math.abs(curr - lasthadithno) < Math.abs(prev - lasthadithno) ? curr : prev);
+
+            console.log(newhadithno)
+
+            myjson[newhadithno] = grade
+
+            lasthadithno = newhadithno
+
+
+
+        }
+
+        fs.writeFileSync(path.join(__dirname,`${indexmap[folder]}.grades.json`),JSON.stringify(myjson,null,4))
+    }
+
+}
+test()
