839382544531776c4c3497ee75112b46a5307004
Fawaz Ahmed <fawazahmed0@hotmail.com>
2023-03-23 03:23:11 +0530

    covert to api format

---
+++ Scripts/bengali/converttoapiformat.js
@@ -0,0 +1,34 @@
+
+const path = require('path')
+const fs = require('fs/promises')
+const fg = require('fast-glob');
+
+async function begin(){
+
+  
+
+  let files = await fg([path.posix.join('.','jsonnew','**')], { dot: true });
+
+  for(let jsonfile of files){
+   let data =  await fs.readFile(jsonfile, {encoding:'utf8'})
+   data = JSON.parse(data)
+
+   let arr = []
+ 
+
+   for(let i=0;i<data.length;i++){
+
+    arr.push(`${data[i].newhadithno} | ${data[i].hadithBengali}`)
+
+   }
+   let bareEdition = jsonfile.split(path.posix.sep).at(-1).replace(/\.json$/gi, '')
+   await fs.writeFile(path.join(__dirname,'jsonnew',`ben-${bareEdition}.txt`), arr.map(e=>e.replace(/\s\s+/gi, ' ').trim()).join('\n'))
+  }
+
+  
+
+
+
+}
+
+begin()
